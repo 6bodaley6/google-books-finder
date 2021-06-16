a654b73b08423b669cb75ca7da77f2d360a21652
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2021-06-15 23:36:57 -0600

    fixed typos in User.js and typeDefs description and password spelled wrong

--- server/models/User.js
+++ server/models/User.js
@@ -30,7 +30,7 @@ const userSchema = new Schema(
 );
 //password hash
 userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('passwrod')) {
+    if (this.isNew || this.isModified('password')) {
         const saltRounds = 10;
         this.password = await bcrypt.hash(this.password, saltRounds);
     }
