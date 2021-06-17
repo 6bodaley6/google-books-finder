576e25682c07931a21b7c5c5e23fab78b944c13e
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2021-06-17 14:29:01 -0600

    third attemp to fix heroku

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -5,6 +5,8 @@ import { useMutation } from '@apollo/react-hooks';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 //add comment for heroku 2econd time
+//add comment for heroku 2econd time
+
 const LoginForm = () => {
     const [userFormData, setUserFormData] = useState({ email: '', password: '' });
     const [validated] = useState(false);
