00478a0816e1292b12715ff0d06ace0e1d9f2851
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2021-06-17 22:11:51 -0600

    removed build pack from heroku website and added just a comment to try to fix heroku

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -9,7 +9,7 @@ const LoginForm = () => {
     const [userFormData, setUserFormData] = useState({ email: '', password: '' });
     const [validated] = useState(false);
     const [showAlert, setShowAlert] = useState(false);
-    //trying to get heroku to work
+    //!! come on heroku work
     const [login, { error }] = useMutation(LOGIN_USER);
 
     const handleInputChange = (event) => {
